45973926b9558844bac64edb60147973b78978f6
miguelphb21 <miguel.s.silva9@aluno.senai.br>
2026-09-15 17:09:28 -0300

    feat: adicionando autenticador no meu sistema

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,10 +37,7 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
-            'name' => config('app.name'),
-            'auth' => [
-                'user' => $request->user(),
-            ],
+            //
         ];
     }
 }
