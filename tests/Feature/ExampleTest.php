a30768ea8e92a6d84f257cb1c9630319f977f374
miguelphb21 <miguel.s.silva9@aluno.senai.br>
2026-09-18 07:01:30 -0300

    test: ajusta o teste de exemplo ao redirecionamento da raiz para o dashboard

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,5 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
+it('redirects the root to the dashboard', function () {
+    $this->get('/')->assertRedirect('/dashboard');
 });
